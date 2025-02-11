ff6abe4b16c2c460538391a9f80f5594bd926373
Thiago Silva Hatanaka <thiagohatanaka18@gmail.com>
2025-02-11 13:19:19 -0300

    preciso trabalhar

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Arquivo Javascript carregado com sucesso!')
+let cart = {};
+
+function addToCart(item, price) {
+	if (cart[item]) {
+		cart[item].quantify += 1;
+	} else {
+		cart[item] = { price: price, quantify: 1};
+	}
+	updateCart();
+}
+
+
