f5c43a4339b606da82f2641f521068bcb43d7686
Thiago Silva Hatanaka <thiagohatanaka18@gmail.com>
2025-02-12 11:56:28 -0300

    finalizado projeto

--- script.js
+++ script.js
@@ -8,5 +8,31 @@ function addToCart(item, price) {
 	}
 	updateCart();
 }
+	function updateCart() {
+		let cartItemsDiv = document.getElementById('cart-items');
+		cartItemsDiv.innerHTML = '';
+		let total = 0;
+
+		for (let item in cart) {
+			let itemTotal = cart[item].price * cart[item].quantify;
+			total += itemTotal;
+			cartItemsDiv.innerHTML += `
+			<div class='cart-item">
+			<span>${item} (x${cart[item].quantify}) - $${itemTotal.toFixed(2)}</span>
+			<button onclick="removeFromCart('${item}')">Remove</button>
+			</div>`;
+		}
+		document.getElementById('total').innerText = total.toFixed(2);
+	}
+
+	function removeFromCart(item) {
+		if (cart[item]) {
+			cart[item].quantify -= 1;
+			if (cart[item].quantify === 0) {
+				delete cart[item];
+			}
+		}
+		updateCart();
+	}
 
 
